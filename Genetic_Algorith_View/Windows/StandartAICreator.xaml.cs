48fea1324cbc36773249e14bda58822060b50327
Бобёр Попёр <Konstantinbobrovski@gmail.com>
2019-10-07 21:29:58 +0300

    Desing changes

--- Genetic_Algorith_View/Windows/StandartAICreator.xaml.cs
+++ Genetic_Algorith_View/Windows/StandartAICreator.xaml.cs
@@ -19,18 +19,22 @@ namespace Genetic_Algorith_View.Windows
     /// </summary>
     public partial class StandartAICreator : Window
     {
+       
+    
         public StandartAICreator()
         {
             InitializeComponent();
 
             for (int i = 0; i < 64; i++)
             {
-                Image image = new Image();
+                var image = new Grid();
+                
+             
                 LogicBlocksGrid.Children.Add(image);
                 image.MouseDown += ImageClick;
                 image.Tag = i;
             }
-
+            RandomizeBlocks();
 
 
             LogicBlocksGrid.Columns = 8;
@@ -40,6 +44,61 @@ namespace Genetic_Algorith_View.Windows
         private void ImageClick(object sender,MouseEventArgs e)
         {
 
+        }
+
+        private void RandomizeBlocks()
+        {
+
+            /*
+        * 0-7 Rotate
+        * 8-15 See
+        * 16-23 Move
+        * 23-31 Catch
+        */
+
+            Random rnd = new Random();
+            
+            for (int i = 0; i < 64; i++)
+            {
+                Brush brush = new ImageBrush();
+              
+                var code = rnd.Next(0, 64);
+                var element = LogicBlocksGrid.Children[i] as Grid;
+                element.Children.Clear();
+                if (code<8)
+                {              
+                  brush=new ImageBrush( new BitmapImage(new Uri("pack://application:,,,/Resources/StandartAI/Rotate" + code + ".png")));      
+                }
+                else if(code<16)
+                {
+                    brush = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Resources/StandartAI/See" + (code-8) + ".png")));
+
+                }
+                else if (code < 24)
+                {
+                    brush = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Resources/StandartAI/Move" + (code - 16) + ".png")));
+
+                }
+                else if (code < 32)
+                {
+                    brush = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Resources/StandartAI/Catch" + (code - 24) + ".png")));
+                }
+                else
+                {
+                    brush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    element.Children.Add(new Label() {
+                        Content = code,
+                        HorizontalContentAlignment =HorizontalAlignment.Center,
+                        VerticalContentAlignment =VerticalAlignment.Center,
+                        FontSize=FontSize*1.5
+                    });
+                }
+
+
+
+
+                element.Background = brush;
+            }
         }
     }
 }
